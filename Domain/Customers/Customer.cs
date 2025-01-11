eea0540cf644e12d5b2aa57824aabdbe9df91f4e
yaroslavx <yakupov.yaroslav@icloud.com>
2025-01-11 14:44:29 +0300

    MAINTAIN: Refactoring

--- Domain/Customers/Customer.cs
+++ Domain/Customers/Customer.cs
@@ -1,10 +1,10 @@
-namespace Domain.Customer;
+namespace Domain.Customers;
 
 public class Customer
 {
-    public Guid Id ( get; private set; )
-
+    public Guid Id { get; private set; }   
+    
     public string Email { get; private set; } = string.Empty;
-
+    
     public string Name { get; private set; } = string.Empty;
 }
--- Domain/Products/Product.cs
+++
@@ -1,6 +0,0 @@
-namespace Domain.Product;
-
-public class Product
-{
-
-}
