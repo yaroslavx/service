bda02f679fa781c387f4725163a8f0a84d5711b3
yaroslavx <yakupov.yaroslav@icloud.com>
2025-01-11 15:26:41 +0300

    MAINTAIN: Completed implementing of Order entity

--- Domain/Orders/Order.cs
+++ Domain/Orders/Order.cs
@@ -1,3 +1,4 @@
+using Domain.Customers;
 using Domain.Products;
 
 namespace Domain.Orders;
@@ -5,14 +6,45 @@ namespace Domain.Orders;
 public class Order
 {
     private readonly HashSet<LineItem> _lineItems = new();
+
+    private Order()
+    {
+    }
     
     public Guid Id { get; private set; }
     
     public Guid CustomerId { get; private set; }
+
+    public static Order Create(Customer customer)
+    {
+        var order = new Order()
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = customer.Id,
+            
+        };
+        
+        return order;
+    }
+    
+    public void Add(Product product)
+    {
+        var LineItem = new LineItem(Guid.NewGuid(), Id, product.Id, product.Price);
+        
+        _lineItems.Add(LineItem);
+    }
 }
 
 public class LineItem
 {
+    internal LineItem(Guid id, Guid orderId, Guid productId, Money price)
+    {
+        Id = id;
+        OrderId = orderId;
+        ProductId = productId;
+        Price = price;
+    }
+    
     public Guid Id { get; private set; }
     
     public Guid OrderId { get; private set; }
